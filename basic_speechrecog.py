cbd3d56aa5d68224d32cfd218fceea2e010896b6
seraphina <47409195+helloseraphina@users.noreply.github.com>
2020-03-15 14:53:50 +0800

    added translator (eng to italian) functionality

--- basic_speechrecog.py
+++ basic_speechrecog.py
@@ -1,20 +1,26 @@
-import speech_recognition as sr
+import speech_recognition as speech
+from googletrans import Translator
 
 #initialise audio recogniser
-audio_recogniser = sr.Recognizer()
+audio_recogniser = speech.Recognizer()
+
+#initialise translator
+translator = Translator()
 
 #initialise audio source
-with sr.Microphone() as source:
+with speech.Microphone() as source:
     print('Say something into the microphone!')
     #takes input from the source and stores it in audio variable
     audio = audio_recogniser.listen(source)
 
     try:
-        #converts audio input into text and prints output
+        #converts audio input into text and prints original and translated output
         voice_input = audio_recogniser.recognize_google(audio)
+        translated_input = translator.translate(voice_input, src = 'en', dest = 'it')
         print(f'You said: {voice_input}')
-    except sr.UnknownValueError:
+        print(f'This is "{translated_input.text}" in Italian!')
+    except speech.UnknownValueError:
         print('Oops! I failed to recognise what you said')
-    except sr.RequestError:
+    except speech.RequestError:
         print('Oops! Google Speech Recognition service is down')
 
